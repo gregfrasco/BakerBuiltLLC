c779fe11d34b0ba78dc9ac6aa8a56673c0c3bc67
Greg Frasco <frascog@wit.edu>
2017-06-13 21:00:11 -0400

    added portfolio page

--- js/index.js
+++ js/index.js
@@ -35,4 +35,25 @@ function onSubmit() {
 
 }
 
+$().ready(function ($) {
+
+  var navButton = $('.nav-toggle');
+  var navMenu = $('.nav-menu');
+
+  navButton.click(() => {
+
+    navButton.toggleClass('is-active');
+    navMenu.toggleClass('is-active');
+  });
+
+  $('.nav-item').each((index, ele) => {
+
+    var element = ele;
+
+    if(window.location.pathname == $(element).attr('href')){
+      $(element).addClass('is-active');
+    }
+  });
+});
+
 new WOW().init();
